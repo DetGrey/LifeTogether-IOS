75073a51697dc52ebc96fd39e68a3f1a3ce2cda7
DetGrey <anenovlar@gmail.com>
2026-06-11 09:45:55 +0200

    Initial Commit

---
+++ LifeTogether-IOS/LifeTogether_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  LifeTogether_IOSApp.swift
+//  LifeTogether-IOS
+//
+//  Created by Ane Novrup Larsen on 11/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct LifeTogether_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
